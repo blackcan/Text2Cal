29ed18d754d052c97194adb24c20eaec9c56818d
blackcan <blackcan@blackcan-2.local>
2012-05-29 22:43:46 +0800

    加入民國年處理

--- Text2Cal/Text2CalAppDelegate.m
+++ Text2Cal/Text2CalAppDelegate.m
@@ -29,7 +29,7 @@ NSMutableArray *dateChanges;
 //測試用
 -(void) awakeFromNib
 {
-    [self setNewEvent:@"2012/5/29  2012/5/30開會"];
+    [self setNewEvent:@"6/1/2012~6/2/2012開會"];
 }
 
 -(NSString*) newEvent
@@ -45,31 +45,6 @@ NSMutableArray *dateChanges;
     endDate = [NSDate dateWithTimeIntervalSinceNow:3600];
     eventName = @"New Event";
     eventPlace = @"";
-    
-//    //取得使用者目前日期時間物件
-//    NSCalendar *usersCalendar =
-//    [[NSLocale currentLocale] objectForKey:NSLocaleCalendar];
-//    
-//    //設定DataComponent要存哪些項目（年月日...）
-//    NSInteger unitFlags = NSYearCalendarUnit | 
-//    NSMonthCalendarUnit |
-//    NSWeekdayCalendarUnit |
-//    NSDayCalendarUnit |  
-//    NSHourCalendarUnit |
-//    NSMinuteCalendarUnit;
-//    
-//    //宣告DateComponent
-//    NSDateComponents *comps = [usersCalendar components:unitFlags fromDate:startDate];
-//    
-//    //取得起始日期時間細項
-//    startYear = [comps year];
-//    startMonth = [comps month];
-//    startDay = [comps day];
-//    startHour = [comps hour];
-//    startMinute = 0;
-    
-    
-    
     dateChanges = [[NSMutableArray alloc] init];
 
     //若傳入字串不為空則開始處理
@@ -78,7 +53,7 @@ NSMutableArray *dateChanges;
         eventName = newEvent;
         
         //處理yyyy/mm/dd
-        NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"(20|21)?[0-9]{2}[- /.,](0?[1-9]|1[012])[- /.,]([12][0-9]|3[01]|0?[1-9])" options:NSRegularExpressionCaseInsensitive error:NULL];
+        NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"(?<=[^0-9]|^)(20|21)?[0-9]{2}[- /.,](0?[1-9]|1[012])[- /.,]([12][0-9]|3[01]|0?[1-9])" options:NSRegularExpressionCaseInsensitive error:NULL];
         NSArray *matches = [regex matchesInString:newEvent options:1 range:NSMakeRange(0, [newEvent length])];
         
         
@@ -86,80 +61,79 @@ NSMutableArray *dateChanges;
         {
             NSRange matchRange = [match range];
             NSString *substring = [newEvent substringWithRange:matchRange];
+            substring = [substring stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
             substring = [substring stringByReplacingOccurrencesOfString:@"-" withString:@"/"];
             substring = [substring stringByReplacingOccurrencesOfString:@" " withString:@"/"];
             substring = [substring stringByReplacingOccurrencesOfString:@"." withString:@"/"];
             substring = [substring stringByReplacingOccurrencesOfString:@"," withString:@"/"];
-            NSDate *date = [NSDate dateWithNaturalLanguageString:substring];
-                        
-            [dateChanges addObject:[NSArray arrayWithObjects:date, NSStringFromRange(matchRange), nil]];
             
+            //將eventName出現的日期資訊刪除
             eventName = [eventName stringByReplacingOccurrencesOfString:substring withString:@""];
+                        
+            [dateChanges addObject:[NSArray arrayWithObjects:substring, NSStringFromRange(matchRange), nil]];
             
         }
         
-//        NSRange range;
-//        NSInteger stringIndex;
+        //處理mm/dd/yyyy
+        regex = [NSRegularExpression regularExpressionWithPattern:@"(?<=[^0-9]|^)(0?[1-9]|1[012])[- /.,]([12][0-9]|3[01]|0?[1-9])[- /.,](20|21)?[0-9]{2}" options:NSRegularExpressionCaseInsensitive error:NULL];
+        matches = [regex matchesInString:newEvent options:1 range:NSMakeRange(0, [newEvent length])];
         
-//        //字串有"明天"
-//        range = [newEvent rangeOfString:@"明天"];
-//        stringIndex = range.location;
-//        if (stringIndex != NSNotFound) 
-//        {
-//            startDay = startDay + 1;
-//            eventName = [eventName stringByReplacingOccurrencesOfString:@"明天" withString:@""];
-//        }
-//        
-//        range = [newEvent rangeOfString:@"一點"];
-//        stringIndex = range.location;
-//        if (stringIndex != NSNotFound) 
-//        {
-//            startHour = 1;
-//            eventName = [eventName stringByReplacingOccurrencesOfString:@"一點" withString:@""];
-//        }
-//        
-//        //字串有"下午"
-//        range = [newEvent rangeOfString:@"下午"];
-//        stringIndex = range.location;
-//        if (stringIndex != NSNotFound) 
-//        {
-//            if (startHour < 12) 
-//            {
-//                startHour = startHour + 12;
-//            }
-//            eventName = [eventName stringByReplacingOccurrencesOfString:@"下午" withString:@""];
-//        }
-//        
-//        //設定結束時間
-//        endYear = startYear;
-//        endMonth = startMonth;
-//        endDay = startDay;
-//        endHour = startHour + 1;
-//        endMinute = startMinute;
-//        
-//        //將日期細項轉換為Date物件
-//        NSDateComponents *comps = [[NSDateComponents alloc] init];
-//        [comps setYear:startYear];
-//        [comps setMonth:startMonth];
-//        [comps setDay:startDay];
-//        [comps setHour:startHour];
-//        [comps setMinute:startMinute];
         
-//        NSCalendar *usersCalendar =
-//        [[NSLocale currentLocale] objectForKey:NSLocaleCalendar];
-//        startDate = [usersCalendar dateFromComponents:comps];
-//        
-//        [comps setYear:endYear];
-//        [comps setMonth:endMonth];
-//        [comps setDay:endDay];
-//        [comps setHour:endHour];
-//        [comps setMinute:endMinute];
-//        endDate = [usersCalendar dateFromComponents:comps];
+        for (NSTextCheckingResult *match in matches) 
+        {
+            NSRange matchRange = [match range];
+            NSString *substring = [newEvent substringWithRange:matchRange];
+            substring = [substring stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+            substring = [substring stringByReplacingOccurrencesOfString:@"-" withString:@"/"];
+            substring = [substring stringByReplacingOccurrencesOfString:@" " withString:@"/"];
+            substring = [substring stringByReplacingOccurrencesOfString:@"." withString:@"/"];
+            substring = [substring stringByReplacingOccurrencesOfString:@"," withString:@"/"];
+            
+            //將eventName出現的日期資訊刪除
+            eventName = [eventName stringByReplacingOccurrencesOfString:substring withString:@""];
+            
+            [dateChanges addObject:[NSArray arrayWithObjects:substring, NSStringFromRange(matchRange), nil]];
+            
+        }
         
+        //處理民國yyy/mm/dd
+        regex = [NSRegularExpression regularExpressionWithPattern:@"[1|2][0-9]{2}[- /.,](0?[1-9]|1[012])[- /.,]([12][0-9]|3[01]|0?[1-9])" options:NSRegularExpressionCaseInsensitive error:NULL];
+        matches = [regex matchesInString:newEvent options:1 range:NSMakeRange(0, [newEvent length])];
+        
+        
+        for (NSTextCheckingResult *match in matches) 
+        {
+            NSRange matchRange = [match range];
+            NSString *substring = [newEvent substringWithRange:matchRange];
+            substring = [substring stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+            substring = [substring stringByReplacingOccurrencesOfString:@"-" withString:@"/"];
+            substring = [substring stringByReplacingOccurrencesOfString:@" " withString:@"/"];
+            substring = [substring stringByReplacingOccurrencesOfString:@"." withString:@"/"];
+            substring = [substring stringByReplacingOccurrencesOfString:@"," withString:@"/"];
+            
+            //將eventName出現的日期資訊刪除
+            eventName = [eventName stringByReplacingOccurrencesOfString:substring withString:@""];
+            
+            //民國年轉成西元年
+            NSString *yearString = [substring substringToIndex:3];
+            yearString = [NSString stringWithFormat:@"%d", [yearString integerValue] + 1911];
+            substring = [NSString stringWithFormat:@"%@%@", yearString, [substring substringFromIndex:3]];
+            
+            [dateChanges addObject:[NSArray arrayWithObjects:substring, NSStringFromRange(matchRange), nil]];
+            
+        }
         
         //處理dateChanges
-        startDate = [[dateChanges objectAtIndex:0] objectAtIndex:0];
-        endDate = [[dateChanges objectAtIndex:1] objectAtIndex:0];
+        if ([dateChanges count] == 1) 
+        {
+            startDate = [NSDate dateWithNaturalLanguageString:[[dateChanges objectAtIndex:0] objectAtIndex:0]];
+            endDate = [NSDate dateWithTimeInterval:3600 sinceDate:startDate];
+        }
+        else if ([dateChanges count] == 2)
+        {
+            startDate = [NSDate dateWithNaturalLanguageString:[[dateChanges objectAtIndex:0] objectAtIndex:0]];
+            endDate = [NSDate dateWithNaturalLanguageString:[[dateChanges objectAtIndex:1] objectAtIndex:0]];
+        }
         
         //新增事件
         iCalApplication *iCal;
